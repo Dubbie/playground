614c3e75e359d80942682bf1b0824d469aea8359
Mihó Dániel <miho.daniel@mdy.hu>
2024-03-05 18:04:12 +0100

    add file controller test

---
+++ tests/Feature/Http/Controllers/FileControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\StoredFile;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class FileControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected bool $seed = true;
+
+    public function test_index()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get(route('file.index'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_download()
+    {
+        $user = User::factory()->create();
+
+        $file = $user->storedFiles()->create([
+            'name' => 'test.txt',
+            'path' => 'test.txt',
+            'type' => 'text/plain',
+            'size' => 1024,
+        ]);
+
+        // Put a mock file in the storage directory
+        Storage::put('test.txt', 'test');
+
+        $response = $this->actingAs($user)->get(route('file.download', ['file' => $file->id]));
+
+        $response->assertDownload('test.txt');
+    }
+}
